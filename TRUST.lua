c00ecb8e58a526b29805cff917b7f28ba3a70c53
bronxkid <doyouthinkiknowthisisfortwitch@gmail.com>
2025-08-01 01:17:05 -0400

    Update TRUST.lua

--- TRUST.lua
+++ TRUST.lua
@@ -19,7 +19,7 @@ local Window = Luna:CreateWindow({
 		Note = "Best Key System Ever! Also, Please Use A HWID Keysystem like Pelican, Luarmor etc. that provide key strings based on your HWID since putting a simple string is very easy to bypass",
 		SaveInRoot = false, -- Enabling will save the key in your RootFolder (YOU MUST HAVE ONE BEFORE ENABLING THIS OPTION)
 		SaveKey = true, -- The user's key will be saved, but if you change the key, they will be unable to use your script
-		Key = {"https://keyguardian.org/services/4680/key?tab=default"}, -- List of keys that will be accepted by the system, please use a system like Pelican or Luarmor that provide key strings based on your HWID since putting a simple string is very easy to bypass
+		Key = {https://keyguardian.org/services/4680/key?tab=default}, -- List of keys that will be accepted by the system, please use a system like Pelican or Luarmor that provide key strings based on your HWID since putting a simple string is very easy to bypass
 		SecondAction = {
 			Enabled = true, -- Set to false if you do not want a second action,
 			Type = "Link", -- Link / Discord.
