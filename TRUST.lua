4d51ab288be9f57406d764f10d5abac994159925
bronxkid <doyouthinkiknowthisisfortwitch@gmail.com>
2025-08-02 01:16:09 -0400

    Update TRUST.lua

--- TRUST.lua
+++ TRUST.lua
@@ -1,9 +1,9 @@
 local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
 local Window = Rayfield:CreateWindow({
-   Name = "Trust Booster Script",
+   Name = "Booster Script",
    Icon = 0, -- Icon in Topbar. Can use Lucide Icons (string) or Roblox Image (number). 0 to use no icon (default).
-   LoadingTitle = "TRUST's booster script",
-   LoadingSubtitle = "Thank you for boosting, TRUST.",
+   LoadingTitle = "booster script",
+   LoadingSubtitle = "Thank you for boosting.",
    ShowText = "Rayfield", -- for mobile users to unhide rayfield, change if you'd like
    Theme = "Default", -- Check https://docs.sirius.menu/rayfield/configuration/themes
 
